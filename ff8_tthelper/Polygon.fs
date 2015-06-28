12e08ed494d082b6faa4e754b461cc1d14ba76c3
Visa Putkinen <visa.putkinen@iki.fi>
2015-06-29 01:21:21 +0300

    Mostly working point in polygon implementation
    Bug: Ray going though a vertex always counted twice

--- ff8_tthelper/Polygon.fs
+++ ff8_tthelper/Polygon.fs
@@ -1,7 +1,5 @@
 ﻿module PolygonModule
 
-open FsUnit
-open NUnit.Framework
 open System.Drawing
 
 type private Orientation =
@@ -12,7 +10,7 @@ type private Orientation =
         if value < 0 then CCW
         else if value = 0 then Colinear
         else CW
-        
+
 type Segment =
     val A: Point
     val B: Point
@@ -36,59 +34,165 @@ type Segment =
      || o3 = Colinear && (other.ContainsColinearPoint this.A)
      || o4 = Colinear && (other.ContainsColinearPoint this.B)
 
-type Polygon(vertices: Point list) =
+    member this.Translated by =
+        Segment(this.A + by, this.B + by)
 
-    member this.Contains (point: Point): bool =
-        true // TODO
+    override this.ToString(): string =
+        sprintf "Seg (%d,%d)->(%d,%d)" this.A.X this.A.Y this.B.X this.B.Y
 
+type Polygon(segments: Segment seq) =
+    let (minX, minY, maxX, maxY) =
+        segments |> Seq.fold (fun (minX,minY,maxX,maxY) s ->
+            (min s.A.X minX, min s.A.Y minY, max s.A.X maxX, max s.A.Y maxY)
+        ) (System.Int32.MaxValue,System.Int32.MaxValue,System.Int32.MinValue,System.Int32.MinValue)
+    let inBoundingBox (p: Point) =
+        p.X >= minX && p.X <= maxX && p.Y >= minY && p.Y <= maxY
 
-[<TestFixture>]
-type ``Segment intersection test`` ()=
-    
-    [<Test>]
-    member x.``Non intersecting segments`` ()=                    //   | |
-        let s1 = Segment((1,0), (1,5))                            //   | |
-        let s2 = Segment((2,0), (2,5))                            //   | |
-        s1.Intersects s2 |> should be False                       //   | |
+    let containsRayCasting (p: Point) =
+        let ray = Segment(p, Point(maxX + 50, p.Y))
+        let intersectCount = segments |> Seq.map ray.Intersects
+                                      |> Seq.sumBy (fun i -> if i then 1 else 0)
+        do System.Console.WriteLine("intersectCount {0}/{1}", intersectCount, (Seq.length segments))
+        intersectCount % 2 = 1
 
-    [<Test>]
-    member x.``Colinear separate segments`` ()=                   //   
-        let s1 = Segment((0,0), (3,0))                            //   
-        let s2 = Segment((4,0), (6,0))                            //   
-        s1.Intersects s2 |> should be False                       //   -----  -----
+    new(vertices: Point seq) =
+        let segments = Seq.append vertices [Seq.head vertices] |> Seq.pairwise |> Seq.map Segment
+        Polygon(segments)
 
-    [<Test>]
-    member x.``Colinear overlapping segments`` ()=                //   
-        let s1 = Segment((0,0), (4,0))                            //   
-        let s2 = Segment((3,0), (6,0))                            //   
-        s1.Intersects s2 |> should be True                        //   ----~~----
+    new(vertices: (int*int) seq) =
+        Polygon(vertices |> Seq.map Point)
 
-    [<Test>]
-    member x.``Equal segments`` ()=                               //
-        let s1 = Segment((0,0), (5,0))                            //
-        let s2 = Segment((0,0), (5,0))                            //
-        s1.Intersects s2 |> should be True                        //   ~~~~~~
+    member this.Contains (p: Point): bool =
+        if not (inBoundingBox p) then false
+        else containsRayCasting p
 
-    [<Test>]
-    member x.``Segments intersecting in the middle`` ()=          //   \  /
-        let s1 = Segment((0,0), (5,5))                            //    \/
-        let s2 = Segment((0,5), (5,0))                            //    /\
-        s1.Intersects s2 |> should be True                        //   /  \
+    member this.Contains (p: int*int): bool =
+        this.Contains(Point(fst p, snd p))
 
-    [<Test>]
-    member x.``Segments intersecting in the first point`` ()=     //   |
-        let s1 = Segment((0,0), (5,0))                            //   |
-        let s2 = Segment((0,0), (0,5))                            //   |
-        s1.Intersects s2 |> should be True                        //   |______
+    override this.ToString(): string =
+        sprintf "Polygon (%A)" segments
 
-    [<Test>]
-    member x.``Segments intersecting in the second point`` ()=    //   |\
-        let s1 = Segment((0,0), (0,5))                            //   | \
-        let s2 = Segment((5,0), (0,5))                            //   |  \
-        s1.Intersects s2 |> should be True                        //   |   \
+module private SegmentTests =
+    open FsUnit
+    open NUnit.Framework
 
-    [<Test>]
-    member x.``Segment touching other segment in the middle`` ()= //      |
-        let s1 = Segment((0,0), (5,0))                            //      |
-        let s2 = Segment((3,0), (3,5))                            //      |
-        s1.Intersects s2 |> should be True                        // _____|___
+    [<TestFixture>]
+    type ``Segment intersection test`` ()=
+
+        [<Test>]
+        member x.``Non intersecting segments`` ()=                    //   | |
+            let s1 = Segment((1,0), (1,5))                            //   | |
+            let s2 = Segment((2,0), (2,5))                            //   | |
+            s1.Intersects s2 |> should be False                       //   | |
+
+        [<Test>]
+        member x.``Colinear separate segments`` ()=                   //   -----  -----
+            let s1 = Segment((0,0), (3,0))                            //
+            let s2 = Segment((4,0), (6,0))                            //
+            s1.Intersects s2 |> should be False                       //
+
+        [<Test>]
+        member x.``Colinear overlapping segments`` ()=                //   ----~~----
+            let s1 = Segment((0,0), (4,0))                            //
+            let s2 = Segment((3,0), (6,0))                            //
+            s1.Intersects s2 |> should be True                        //
+
+        [<Test>]
+        member x.``Equal segments`` ()=                               //   ~~~~~~
+            let s1 = Segment((0,0), (5,0))                            //
+            let s2 = Segment((0,0), (5,0))                            //
+            s1.Intersects s2 |> should be True                        //
+
+        [<Test>]
+        member x.``Segments intersecting in the middle`` ()=          //   \  /
+            let s1 = Segment((0,0), (5,5))                            //    \/
+            let s2 = Segment((0,5), (5,0))                            //    /\
+            s1.Intersects s2 |> should be True                        //   /  \
+
+        [<Test>]
+        member x.``Segments intersecting in the first point`` ()=     //    ______
+            let s1 = Segment((0,0), (5,0))                            //   |
+            let s2 = Segment((0,0), (0,5))                            //   |
+            s1.Intersects s2 |> should be True                        //   |
+
+        [<Test>]
+        member x.``Segments intersecting in the second point`` ()=    //   |   /
+            let s1 = Segment((0,0), (0,5))                            //   |  /
+            let s2 = Segment((5,0), (0,5))                            //   | /
+            s1.Intersects s2 |> should be True                        //   |/
+
+        [<Test>]
+        member x.``Segment touching other segment in the middle`` ()= // _________
+            let s1 = Segment((0,0), (5,0))                            //      |
+            let s2 = Segment((3,0), (3,5))                            //      |
+            s1.Intersects s2 |> should be True                        //      |
+
+module PolygonTests =
+    open FsUnit
+    open NUnit.Framework
+    open NUnit.Framework.Constraints
+    open System.Drawing
+
+    type PointInPolygonConstraint(polygon: Polygon) =
+        inherit Constraint()
+        let mutable point: (int*int) = 0,0
+
+        override this.Matches(actual: obj): bool =
+            let givenPoint = actual :?> int*int
+            let pointInPolygon = polygon.Contains(givenPoint)
+            let actualSuffix = if pointInPolygon then "is in polygon" else "is not in polygon"
+            point <- givenPoint 
+            this.actual <- sprintf "point %A %s" givenPoint actualSuffix
+            pointInPolygon
+            
+        override this.WriteDescriptionTo(writer: MessageWriter) =
+            writer.Write(sprintf "point %A is in polygon" point)
+
+    let InPolygon(polygon) = PointInPolygonConstraint(polygon)
+
+    [<TestFixture>]
+    type ``Polygon test`` ()=
+        let simple = Polygon([20,30; 25,25; 30,50; 40,20; 50,15; 50,55; 30,60; 15,50])
+        let square3by3 = Polygon([0,0; 2,0; 2,2; 0,2])
+        let nr8 =
+            let outline = [30,10;40,20;40,30;35,40;40,50;40,60;30,70;20,60;20,50;25,40;20,30;20,20;30,10]
+                            |> Seq.pairwise |> Seq.map Segment
+            let upperHole = [30,15;35,20;35,30;30,35;25,30;25,20;30,15] |> Seq.pairwise |> Seq.map Segment
+            let lowerHole: Segment seq = upperHole |> Seq.map (fun s -> s.Translated(Size(0,30)))
+            Polygon (Seq.concat [outline; upperHole; lowerHole])
+
+        let shouldBeIn polygon p = p |> should be (InPolygon polygon)
+        let shouldNotBeIn polygon p = p |> should not' (be (InPolygon polygon))
+
+        [<Test>] member x.``in, 1 isect``()=                        (45,40) |> shouldBeIn    simple
+        [<Test>] member x.``in, 3 isects``()=                       (22,40) |> shouldBeIn    simple
+        [<Test>] member x.``in, isect through concave vertex``()=   (22,50) |> shouldBeIn    simple
+        [<Test>] member x.``out, on concave vertex``()=             (30,50) |> shouldNotBeIn simple
+        [<Test>] member x.``out, outside bounding box``()=          ( 5,40) |> shouldNotBeIn simple
+        [<Test>] member x.``out, inside bounding box, 0 isects``()= (40,58) |> shouldNotBeIn simple
+        [<Test>] member x.``out, inside bounding box, 2 isects``()= (17,40) |> shouldNotBeIn simple
+        [<Test>] member x.``out, inside concave indent``()=         (30,40) |> shouldNotBeIn simple
+
+        [<Test>] member x.``in, in square``()=                        (1,1) |> shouldBeIn    square3by3
+        [<Test>] member x.``out, on square top left corner``()=       (0,0) |> shouldNotBeIn square3by3
+        [<Test>] member x.``out, on square bottom right corner``()=   (2,2) |> shouldNotBeIn square3by3
+        [<Test>] member x.``out, on square left edge``()=             (0,1) |> shouldNotBeIn square3by3
+        [<Test>] member x.``out, on square right edge``()=            (2,1) |> shouldNotBeIn square3by3
+        [<Test>] member x.``out, on square bottom edge``()=           (1,2) |> shouldNotBeIn square3by3
+
+        [<Test>] member x.``in, in the bottom left leg of 8``()=    (22,55) |> shouldBeIn    nr8
+        [<Test>] member x.``in, in the top right hand of 8``()=     (37,25) |> shouldBeIn    nr8
+        [<Test>] member x.``in, ray exits through vertex``()=       (30,40) |> shouldBeIn    nr8
+        [<Test>] member x.``in, on leftmost segment``()=            (20,25) |> shouldBeIn    nr8
+        [<Test>] member x.``in, on rightmost segment``()=           (40,25) |> shouldBeIn    nr8
+        [<Test>] member x.``out, in the left crack of 8``()=        (21,33) |> shouldNotBeIn nr8
+        [<Test>] member x.``out, in the top hole of 8``()=          (30,25) |> shouldNotBeIn nr8
+
+        [<Test>]
+        member x.``draw 8``()=
+            let bitmap = new Bitmap(30, 70, Imaging.PixelFormat.Format24bppRgb)
+            [ for y in 0..bitmap.Height-1 do
+                for x in 0..bitmap.Width-1 do
+                    if nr8.Contains(Point(x+15,y+5)) then yield (x,y,Color.White) ]
+                |> List.iter bitmap.SetPixel
+            bitmap.Save(@"D:\temp\nr8.png", Imaging.ImageFormat.Png)
