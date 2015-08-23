1ee85ecd5f364043a67f0d5457ab7afbf73f4b30
Visa Putkinen <visa.putkinen@iki.fi>
2015-08-23 16:16:56 +0300

    Refactor main()

--- ff8_tthelper/Program.fs
+++ ff8_tthelper/Program.fs
@@ -13,39 +13,50 @@ let bootstrap () =
     //Bootstrap.saveEmptyPlayGridSlotElementBitmaps()
     ()
 
-[<EntryPoint>]
-let main argv = 
+let playGame initState =
+    let mutable state = initState
     let sw = new System.Diagnostics.Stopwatch()
-    sw.Start()
+    while not (AI.isTerminalNode state) do
+        printfn "%O-------------------------------------- %d" state (AI.evaluateNode state)
+        sw.Restart()
+        let (move, value) = AI.getBestMove state 9
+        printfn "Best move is %d -> (%d,%d) with value %d (took %d ms)" (fst move)
+                                                                        (snd move / 3) (snd move % 3)
+                                                                        value
+                                                                        sw.ElapsedMilliseconds
+        state <- AI.executeMove state move
 
+    printfn "%O-------------------------------------- %d\n" state (AI.evaluateNode state)
+
+let watchScreenshotDir () =
     let steamScreenshotDir = @"D:\Program Files\Steam\userdata\33243684\760\remote\39150\screenshots"
     let watcher = new System.IO.FileSystemWatcher(steamScreenshotDir, "????-??-??_?????.jpg")
-    let roundSw = new System.Diagnostics.Stopwatch()
+    let sw = new System.Diagnostics.Stopwatch()
     while true do
         printfn "Waiting for new screenshot..."
         let changedResult = watcher.WaitForChanged(System.IO.WatcherChangeTypes.Created)
         printfn "#######################################"
         printfn "#### DETECTED %s" changedResult.Name
         printfn "#######################################"
-        roundSw.Restart()
+        sw.Restart()
 
         let screenshot = new Bitmap(steamScreenshotDir + @"\" + changedResult.Name)
-        let mutable state = readGameState screenshot
+        let state = readGameState screenshot
         screenshot.Dispose()
-        printfn "GameState read in %d ms" roundSw.ElapsedMilliseconds
+        printfn "GameState read in %d ms" sw.ElapsedMilliseconds
 
-        while not (AI.isTerminalNode state) do
-            printfn "%O-------------------------------------- %d" state (AI.evaluateNode state)
-            roundSw.Restart()
-            let (move, value) = AI.getBestMove state 9
-            printfn "Best move is %d -> (%d,%d) with value %d (took %d ms)" (fst move) (snd move / 3) (snd move % 3) value roundSw.ElapsedMilliseconds
-            state <- AI.executeMove state move
+        playGame state
 
-        printfn "%O-------------------------------------- %d\n" state (AI.evaluateNode state)
+    watcher.Dispose()
+
+[<EntryPoint>]
+let main argv = 
+    let sw = new System.Diagnostics.Stopwatch()
+    sw.Start()
+
+    watchScreenshotDir()
 
     sw.Stop()
     printfn "Time elapsed: %A" sw.Elapsed
-
-    watcher.Dispose()
 
     0
